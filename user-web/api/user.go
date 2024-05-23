6da05757cce6d9791e91c1850f608970c8f468fd
HyxiaoGe <18889592303@163.com>
2024-05-24 00:44:06 +0800

    go & python project first ping/pong

--- user-web/api/user.go
+++ user-web/api/user.go
@@ -1,7 +1,77 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"net/http"
+	"shop-api/user-web/proto"
+)
+
+func HandlerGrpcErrorToHttp(err error, c *gin.Context) {
+	if err != nil {
+		if e, ok := status.FromError(err); ok {
+			switch e.Code() {
+			case codes.NotFound:
+				c.JSON(http.StatusNotFound, gin.H{
+					"msg": e.Message(),
+				})
+			case codes.Internal:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg": "内部错误",
+				})
+			case codes.InvalidArgument:
+				c.JSON(http.StatusBadRequest, gin.H{
+					"msg": "参数错误",
+				})
+			case codes.Unavailable:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg": "服务不可用",
+				})
+			default:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg": "其他错误",
+				})
+			}
+			return
+		}
+	}
+}
 
 func GetUserList(ctx *gin.Context) {
-	// code here
+	ip := "127.0.0.1"
+	port := 50051
+	userConn, err := grpc.Dial(fmt.Sprintf("%s:%d", ip, port), grpc.WithInsecure())
+	if err != nil {
+		zap.S().Errorf("连接用户服务失败: %v", err)
+		return
+	}
+	//defer userConn.Close()
+	userClient := proto.NewUserClient(userConn)
+	rsp, err := userClient.GetUserList(context.Background(), &proto.PageInfo{
+		Page:     1,
+		PageSize: 10,
+	})
+	if err != nil {
+		zap.S().Errorw("获取用户列表失败", "msg", err.Error())
+		HandlerGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	result := make([]interface{}, 0)
+	for _, value := range rsp.Data {
+		data := make(map[string]interface{})
+		data["id"] = value.Id
+		data["name"] = value.Nickname
+		data["birthday"] = value.Birthday
+		data["gender"] = value.Gender
+		data["mobile"] = value.Mobile
+
+		result = append(result, data)
+	}
+	ctx.JSON(http.StatusOK, result)
 }
