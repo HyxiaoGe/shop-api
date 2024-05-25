ce859701d1ec5d1ecb5671a819b4b8bbd1d7cc62
HyxiaoGe <18889592303@163.com>
2024-05-25 16:10:07 +0800

    add permission check & handler cors

--- user-web/router/user.go
+++ user-web/router/user.go
@@ -4,13 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 	"shop-api/user-web/api"
+	"shop-api/user-web/middlewares"
 )
 
 func InitUserRouter(Router *gin.RouterGroup) {
 	UserRouter := Router.Group("user")
 	zap.S().Info("初始化用户路由")
 	{
-		UserRouter.GET("list", api.GetUserList)
+		UserRouter.GET("list", middlewares.JWTAuth(), middlewares.IsAdminAuth(), api.GetUserList)
 		UserRouter.POST("pwd_login", api.LoginByPassword)
 	}
 }
